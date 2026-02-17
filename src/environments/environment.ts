eb9b982ed4dd0fd086462723de78fcef048cc39d
Samueljustiniani <samuel.justiniani.lancho@vallegrande.edu.pe>
2026-02-16 21:20:58 -0500

    asde

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8085/v1/api'
+  apiUrl: 'https://backend-spas.onrender.com/v1/api'
   // Producción: 'https://backend-spas.onrender.com/v1/api' http://localhost:8085/v1/api 
 };
